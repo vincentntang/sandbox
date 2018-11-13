6a37bf80786b38f47c52642b0092e234f7089417
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-11-13 07:25:25 -0500

    updated speedtype with comments

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is where I put mini reference projects from exercises that only require a frontend
 
-- [Speed Typer](https://vincentntang.github.io/sandbox/speed-typer/) - Tests how fast you can copy pasta something
+- [Speed Typer](https://vincentntang.github.io/sandbox/speed-typer/) - Tests how fast you can copy pasta something. Lots of algebra
 - [Todo App](https://vincentntang.github.io/sandbox/todo/) - Vanilla JS, TodoMVC app from Gordon's Watchandcode.com
 - [Canvas Balls](https://vincentntang.github.io/sandbox/canvas-balls/) - Object Oriented Design with HTML Canvas
 - [Drumkit](https://vincentntang.github.io/sandbox/drumkit/) - A drumkit that plays mp3 files with event handlers
--- speed-typer/script.js
+++ speed-typer/script.js
@@ -4,39 +4,45 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-var timer = [0,0,0,0]; //minutes,seconds, hundreths, thousandths
+var timer = [0, 0, 0, 0]; //minutes,seconds, hundreths, thousandths [3,2,1,0]
 var interval;
 var timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
-  if (time <= 9){
+  if (time <= 9) {
     time = "0" + time;
   }
   return time;
 }
 
+// timer[0] = minutes
+// timer[1] = seconds
+// timer[2] = hundredth second
+// timer[3] = thousandth second
+
 // Run a standard minute/second/hundredths timer:
-function runTimer(){
+function runTimer() {
   let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
   theTimer.innerHTML = currentTime
-  timer[3]++; //update thousandths
+  timer[3]++; // thousandth seconds updated
 
-  timer[0] = Math.floor((timer[3]/100)/60);
-  timer[1] = Math.floor((timer[3]/100)-(timer[0]*60));
-  timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0]*6000));
+  timer[0] = Math.floor((timer[3] / 100) / 60); // one minute = 6000 thousandth second
+  timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60)); // one second = 100 thousandth seconds, 60 seconds = 1 minute
+  timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+  // one hundredth second, one hundredth second = 100 thousandth seconds, one minute = 6000 thousandth seconds
 }
 
 // Match the text entered with the provided text on the page:
-function spellCheck(){
+function spellCheck() {
   let textEntered = testArea.value; //grabs input
-  let originTextMatch = originText.substring(0,textEntered.length); //Original Value
+  let originTextMatch = originText.substring(0, textEntered.length); //Original Value
 
-  if (textEntered == originText){
+  if (textEntered == originText) {
     clearInterval(interval);
     testWrapper.style.borderColor = "#429890"; // teal
   } else {
-    if (textEntered == originTextMatch){
+    if (textEntered == originTextMatch) {
       testWrapper.style.borderColor = "#65CCF3"; //green
     } else {
       testWrapper.style.borderColor = "#E95D0F"; // orange
@@ -59,7 +65,7 @@ function start() {
 function reset() {
   clearInterval(interval);
   interval = null;
-  timer = [0,0,0,0];
+  timer = [0, 0, 0, 0];
   timerRunning = false;
 
   //Reset values
@@ -71,6 +77,6 @@ function reset() {
 }
 
 // Event listeners for keyboard input and the reset button:
-testArea.addEventListener("keypress",start , false);
+testArea.addEventListener("keypress", start, false);
 testArea.addEventListener("keyup", spellCheck, false); //when key is let go
 resetButton.addEventListener("click", reset, false);
